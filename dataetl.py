551eabde8d378964b9e2db323d2ba58b662f611b
Charlotte <huxiaoman707@gmail.com>
2023-07-30 18:25:30 +0800

    Update dataetl.py

--- dataetl.py
+++ dataetl.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 30/7/2023
+# @Author  : huxiaoman
+# @File    : dataetl.py
+
 #import libraries
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
