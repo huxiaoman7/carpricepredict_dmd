e353763e458acadf562d52ca33fe7838b7510d81
Charlotte <huxiaoman707@gmail.com>
2023-07-30 18:05:20 +0800

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,6 +1,17 @@
 import scipy
 from scipy.stats.stats import pearsonr
 
+#分析因变量Price
+plt.title('Car Price Spread')
+sns.boxplot(y=data.price)
+plt.show()
+print(data.price.describe())
+plt.title('Car Price Distribution Plot')
+sns.distplot(data.price)
+plt.show()
+print(data.price.describe())
+
+#分析自变量
 def pairplot(x_axis,y_axis) :
     sns.pairplot(data,x_vars=x_axis,y_vars=y_axis,height=4,aspect=1,kind="scatter")
     plt.show()
@@ -26,7 +37,7 @@ pearson_coeff, p_value = pearsonr(p3,data['price'])
 print('Correlation coefficient between Price and boreratio: ',pearson_coeff*100,'%')
 
 
-#######
+#######Price VS carlength - carwidth - carheight
 x_vars=['carlength','carwidth', 'carheight']
 pairplot(x_vars,y_vars)
 print("Carlength and Carwidth are more correlated than carheight which is more spread out but positive.")
@@ -45,7 +56,7 @@ print('Correlation coefficient between Price and carwidth: ',pearson_coeff*100,'
 pearson_coeff, p_value = pearsonr(p3,data['price'])
 print('Correlation coefficient between Price and carheight: ',pearson_coeff*100,'%')
 
-#####
+#####Price VS enginesize - horsepower - stroke
 x_vars=['enginesize','horsepower','stroke']
 pairplot(x_vars,y_vars)
 print("Enginesize and Horsepower are positively correlated, but Stroke is more spread out (may not be related).")
@@ -66,7 +77,7 @@ print('Correlation coefficient between Price and stroke: ',pearson_coeff*100,'%'
 
 
 
-####
+####Price VS compressionratio - peakrpm - symboling
 x_vars=['compressionratio','peakrpm',"symboling"]
 pairplot(x_vars,y_vars)
 print("Compressionratio, Peakrpm and symboling are not correlated.")
@@ -84,3 +95,176 @@ print('Correlation coefficient between Price and peakrpm: ',pearson_coeff*100,'%
 
 pearson_coeff, p_value = pearsonr(p3,data['price'])
 print('Correlation coefficient between Price and symboling: ',pearson_coeff*100,'%')
+
+#Price VS citympg - highwaympg
+x_vars=['citympg', 'highwaympg']
+pairplot(x_vars,y_vars)
+print('Citympg & Highwaympg are negatively correlated.\nThe more prices get lower, the higher the distances get, which means that the cheapest cars have better mileage than expensive cars.')
+
+p1=data['citympg']
+p2=data['highwaympg']
+
+pearson_coeff, p_value = pearsonr(p1,data['price'])
+print('\nWe can make sure of this by looking at the Coefficient of Correlation')
+print('\nCorrelation coefficient between Price and citympg: ',pearson_coeff*100,'%')
+
+pearson_coeff, p_value = pearsonr(p2,data['price'])
+print('Correlation coefficient between Price and highwaympg: ',pearson_coeff*100,'%')
+
+#检验上述相关自变量与价格Price之间是否具有高相关性
+def heatmap(x,y,dataframe):
+	plt.figure(figsize=(x,y))
+	n_variables = ['symboling','wheelbase','carlength', 'carwidth', 'carheight', 'curbweight', 'enginesize', 'boreratio', 'stroke','compressionratio', 'horsepower', 'peakrpm', 'citympg', 'highwaympg','price']
+	pc = dataframe[n_variables].corr(method='pearson')
+	cols = n_variables
+	sns.heatmap(pc,annot=True,yticklabels=cols,xticklabels=cols,annot_kws={'size':10},cmap='coolwarm')
+	plt.show()
+  
+heatmap(20,12,data)
+
+#i. 检查与汽车尺寸相关的变量之间的相关性，如weight, height 等
+def heatmap1(x,y,dimention,dataframe):
+	plt.figure(figsize=(x,y))
+	n_variables = dimention
+	pc = dataframe[dimention].corr(method='pearson')
+	cols= dimention
+	sns.heatmap(pc,annot=True,yticklabels=cols,xticklabels=cols,annot_kws={'size':10},cmap='coolwarm')
+	plt.show()
+    
+    
+dimension_col_list = ['wheelbase', 'carlength', 'carwidth','curbweight']
+heatmap1(10,10,dimension_col_list,data.filter(dimension_col_list))
+
+dimension_col_list = ['enginesize','boreratio','horsepower']
+heatmap1(10,10,dimension_col_list,data.filter(dimension_col_list))
+
+
+######分析类别变量
+plt.figure(figsize=(20,9))
+
+plt.xticks(rotation = 90)
+order = data1['CompanyName'].value_counts(ascending=False).index
+sns.countplot(x='CompanyName', data=data1, order=order)
+
+plt.show()
+
+#Price VS fueltype
+plt.figure(figsize=(20, 12))
+
+plt.subplot(2,3,1)
+sns.boxplot(x = 'fueltype', y = 'price', data = data)
+
+plt.subplot(2,3,2)
+plt.title('Fuel Type Histogram')
+order = data1['fueltype'].value_counts(ascending=False).index
+sns.countplot(x='fueltype', data=data, order=order)
+
+plt.show()
+
+
+#Price VS aspiration
+plt.figure(figsize=(20, 12))
+
+plt.subplot(2,3,1)
+sns.boxplot(x = 'aspiration', y = 'price', data = data)
+
+plt.subplot(2,3,2)
+plt.title('Aspiration Histogram')
+order = data1['aspiration'].value_counts(ascending=False).index
+sns.countplot(x='aspiration', data=data, order=order)
+
+plt.show()
+
+#Price VS doornumber
+plt.figure(figsize=(20, 12))
+
+plt.subplot(2,3,1)
+sns.boxplot(x = 'doornumber', y = 'price', data = data)
+
+plt.subplot(2,3,2)
+plt.title('Door Number Histogram')
+order = data1['doornumber'].value_counts(ascending=False).index
+sns.countplot(x='doornumber', data=data, order=order)
+
+plt.show()
+
+#Price VS enginelocation
+plt.figure(figsize=(20, 12))
+
+plt.subplot(2,3,1)
+sns.boxplot(x = 'enginelocation', y = 'price', data = data)
+
+plt.subplot(2,3,2)
+plt.title('Engine Location Histogram')
+order = data1['enginelocation'].value_counts(ascending=False).index
+sns.countplot(x='enginelocation', data=data, order=order)
+
+plt.show()
+
+#Price VS carbody
+plt.subplot(2,3,1)
+sns.boxplot(x='carbody',y='price',data = data)
+
+plt.subplot(2,3,2)
+plt.title('Car Body Histogram')
+order = data1['carbody'].value_counts(ascending=False).index
+sns.countplot(x='carbody', data=data, order=order)
+
+plt.show()
+
+#Price VS fuelsystem
+plt.subplot(2,3,1)
+sns.boxplot(x='fuelsystem',y='price',data = data)
+
+plt.subplot(2,3,2)
+plt.title('Fuel System Histogram')
+order = data['fuelsystem'].value_counts(ascending=False).index
+sns.countplot(x='fuelsystem', data=data, order=order)
+
+plt.show()
+
+#Price VS enginetype
+plt.subplot(2,3,1)
+sns.boxplot(x='enginetype',y='price',data = data)
+
+plt.subplot(2,3,2)
+plt.title('Engine Type Histogram')
+order = data1['enginetype'].value_counts(ascending=False).index
+sns.countplot(x='enginetype', data=data, order=order)
+
+plt.show()
+
+#Price VS cylindernumber
+plt.subplot(2,3,1)
+sns.boxplot(x='cylindernumber',y='price',data = data)
+
+plt.subplot(2,3,2)
+plt.title('Cylinder Number Histogram')
+order = data1['cylindernumber'].value_counts(ascending=False).index
+sns.countplot(x='cylindernumber', data=data, order=order)
+
+plt.show()
+
+#Price VS drivewheel
+plt.subplot(2,3,1)
+sns.boxplot(x = 'drivewheel', y = 'price', data = data)
+
+plt.subplot(2,3,2)
+plt.title('DriveWheel Histogram')
+order = data1['drivewheel'].value_counts(ascending=False).index
+sns.countplot(x='drivewheel', data=data, order=order)
+
+plt.show()
+
+#Price VS symboling
+plt.subplot(2,3,1)
+sns.boxplot(x=data.symboling, y=data.price)
+
+
+plt.subplot(2,3,2)
+plt.title('Symboling Histogram')
+order = data1['symboling'].value_counts(ascending=False).index
+sns.countplot(x='symboling', data=data, order=order)
+
+plt.show()
+
