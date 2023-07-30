b54f6b43f4b151c7e65dbc9fbbadc251c56f1a93
Charlotte <huxiaoman707@gmail.com>
2023-07-30 14:00:55 +0800

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -24,3 +24,63 @@ print('Correlation coefficient between Price and curbweight:',pearson_coeff*100,
 
 pearson_coeff, p_value = pearsonr(p3,data['price'])
 print('Correlation coefficient between Price and boreratio: ',pearson_coeff*100,'%')
+
+
+#######
+x_vars=['carlength','carwidth', 'carheight']
+pairplot(x_vars,y_vars)
+print("Carlength and Carwidth are more correlated than carheight which is more spread out but positive.")
+
+p1=data['carlength']
+p2=data['carwidth']
+p3=data['carheight']
+
+pearson_coeff, p_value = pearsonr(p1,data['price'])
+print('\nWe can make sure of this by looking at the Coefficient of Correlation')
+print('\nCorrelation coefficient between Price and carlength:',pearson_coeff*100,'%')
+
+pearson_coeff, p_value = pearsonr(p2,data['price'])
+print('Correlation coefficient between Price and carwidth: ',pearson_coeff*100,'%')
+
+pearson_coeff, p_value = pearsonr(p3,data['price'])
+print('Correlation coefficient between Price and carheight: ',pearson_coeff*100,'%')
+
+#####
+x_vars=['enginesize','horsepower','stroke']
+pairplot(x_vars,y_vars)
+print("Enginesize and Horsepower are positively correlated, but Stroke is more spread out (may not be related).")
+
+p1=data['enginesize']
+p2=data['horsepower']
+p3=data['stroke']
+
+pearson_coeff, p_value = pearsonr(p1,data['price'])
+print('\nWe can make sure of this by looking at the Coefficient of Correlation')
+print('\nCorrelation coefficient between Price and enginesize: ',pearson_coeff*100,'%')
+
+pearson_coeff, p_value = pearsonr(p2,data['price'])
+print('Correlation coefficient between Price and horsepower: ',pearson_coeff*100,'%')
+
+pearson_coeff, p_value = pearsonr(p3,data['price'])
+print('Correlation coefficient between Price and stroke: ',pearson_coeff*100,'%')
+
+
+
+####
+x_vars=['compressionratio','peakrpm',"symboling"]
+pairplot(x_vars,y_vars)
+print("Compressionratio, Peakrpm and symboling are not correlated.")
+
+p1=data['compressionratio']
+p2=data['peakrpm']
+p3=data['symboling']
+
+pearson_coeff, p_value = pearsonr(p1,data['price'])
+print('\nWe can make sure of this by looking at the Coefficient of Correlation')
+print('\nCorrelation coefficient between Price and compressionratio: ',pearson_coeff*100,'%')
+
+pearson_coeff, p_value = pearsonr(p2,data['price'])
+print('Correlation coefficient between Price and peakrpm: ',pearson_coeff*100,'%')
+
+pearson_coeff, p_value = pearsonr(p3,data['price'])
+print('Correlation coefficient between Price and symboling: ',pearson_coeff*100,'%')
