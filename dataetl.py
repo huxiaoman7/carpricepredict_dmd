cc48133c042c92e7e8eb8926ddd93dcfd19b3d57
Charlotte <huxiaoman707@gmail.com>
2023-07-30 18:00:47 +0800

    Update dataetl.py

--- dataetl.py
+++ dataetl.py
@@ -15,7 +15,21 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 import matplotlib.pyplot as plt
 from scipy.stats import pearsonr
-data = pd.read_csv('/Users/huxiaoman/tsinghua/dmd/carprice.csv')
+
+# 导入数据
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
+
+pd.set_option('display.max_columns', None)
+plt.rcParams['figure.figsize'] = (20.0, 10.0)
+data = pd.read_csv('./carprice.csv')
+print("Dimension of our data set is: ")
+print(data.shape)
+
 ################数据基本信息######################
 data.head(5)
 data.shape
@@ -27,7 +41,10 @@ CompanyName = data['CarName'].apply(lambda x : x.split(' ')[0])
 data.insert(3,"CompanyName",CompanyName)
 data.drop(['CarName'],axis=1,inplace=True)
 data.drop(['car_ID'],axis=1,inplace=True)
+data1=data ##方便后面分析类别变量
 data.head()
+
+##################获取变量类型##########################
 def get_variable_type(element) :
     if element==0:
         return "Not Known"
@@ -38,33 +55,33 @@ def get_variable_type(element) :
 
 ################检测类别变量是否有拼写错误################
 def predict_variable_type(metadata_matrix):
-    metadata_matrix["Variable_Type"] = metadata_matrix["Valeurs_Uniques_Count"].apply(get_variable_type).astype(str)
+    metadata_matrix["Variable_Type"] = metadata_matrix["Value_count"].apply(get_variable_type).astype(str)
     metadata_matrix["frequency"] = metadata_matrix["Null_Count"] - metadata_matrix["Null_Count"]
     metadata_matrix["frequency"].astype(int)
     return metadata_matrix 
 
 def get_meta_data(dataframe) :
     metadata_matrix = pd.DataFrame({
-                    'Datatype' : dataframe.dtypes.astype(str), # types de données de colonnes
-                    'Non_Null_Count': dataframe.count(axis = 0).astype(int), # nombre total d'éléments dans les colonnes
-                    'Null_Count': dataframe.isnull().sum().astype(int), # total des valeurs nulles dans les colonnes
-                    'Null_Percentage': dataframe.isnull().sum()/len(dataframe) * 100, # pourcentage de valeurs nulles
-                    'Valeurs_Uniques_Count': dataframe.nunique().astype(int) # nombre de valeurs uniques
+                    'Datatype' : dataframe.dtypes.astype(str), 
+                    'Non_Null_Count': dataframe.count(axis = 0).astype(int), 
+                    'Null_Count': dataframe.isnull().sum().astype(int), 
+                    'Null_Percentage': dataframe.isnull().sum()/len(dataframe) * 100, 
+                    'Value_count': dataframe.nunique().astype(int) 
                      })
     
     metadata_matrix = predict_variable_type(metadata_matrix)
     return metadata_matrix
 
 def list_potential_categorical_type(dataframe,data) :
-    print("*********colonnes de type de données catégoriques potentielles*********")
+    print("**************潜在的类别数据类型列*****************")
     metadata_matrix_categorical = dataframe[dataframe["Variable_Type"] == "Categorical"]
     
     length = len(metadata_matrix_categorical)
     if length == 0 :
-        print("Aucune colonne catégorique dans un jeu de données donné.")  
+        print("给定数据集中没有分类列。")  
     else :    
-        metadata_matrix_categorical = metadata_matrix_categorical.filter(["Datatype","Valeurs_Uniques_Count"])
-        metadata_matrix_categorical.sort_values(["Valeurs_Uniques_Count"], axis=0,ascending=False, inplace=True)
+        metadata_matrix_categorical = metadata_matrix_categorical.filter(["Datatype","Value_count"])
+        metadata_matrix_categorical.sort_values(["Value_count"], axis=0,ascending=False, inplace=True)
         col_to_check = metadata_matrix_categorical.index.tolist()
         name_list = []
         values_list = []
@@ -73,7 +90,25 @@ def list_potential_categorical_type(dataframe,data) :
             name_list.append(name)
             values_list.append(data[name].unique())
         
-        temp = pd.DataFrame({"index":name_list,"Valeurs_Uniques":values_list})
+        temp = pd.DataFrame({"index":name_list,"Value":values_list})
         metadata_matrix_categorical = metadata_matrix_categorical.reset_index()
         metadata_matrix_categorical = pd.merge(metadata_matrix_categorical,temp,how='inner',on='index')
         print(metadata_matrix_categorical.set_index("index"))
+
+metadata = get_meta_data(data)
+list_potential_categorical_type(metadata,data)
+
+##输出CompanyName所有名称
+data.CompanyName.unique()
+
+##将错误名称改为正确名称
+data = data.replace(to_replace ="maxda", value ="mazda") 
+data = data.replace(to_replace ="porcshce", value ="porsche") 
+data = data.replace(to_replace ="toyouta", value ="toyota") 
+data = data.replace(to_replace ="vokswagen", value ="volkswagen") 
+data = data.replace(to_replace ="vw", value ="volkswagen")
+data = data.replace(to_replace ="Nissan", value ="nissan")
+data.CompanyName.unique()
+
+#处理好后的CompanyName：
+data.CompanyName.unique()
